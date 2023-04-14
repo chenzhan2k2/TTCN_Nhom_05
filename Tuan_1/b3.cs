962891ef160d5c5dd6bedd2352df045d7393fb8a
chenzhan2k2 <100663760+chenzhan2k2@users.noreply.github.com>
2023-04-14 22:15:17 +0700

    Tuan_1
    Tuan_1

---
+++ Tuan_1/b3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        class Program {
+        static void Main(string[] args) {
+            double length = 7.5;
+            double width = 3.8;
+            Console.WriteLine("Area = " + length * width);
+        }
+    }
+}
+}
