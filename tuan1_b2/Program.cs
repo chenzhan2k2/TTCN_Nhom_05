badf88ba119cc8c7de2c75d5a1d774534013d378
chenzhan2k2 <100663760+chenzhan2k2@users.noreply.github.com>
2023-04-14 21:57:31 +0700

    first commit

---
+++ tuan1_b2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+       class Program {
+        static void Main(string[] args) {
+            double a = 10.5;
+            int b = 7;
+            Console.WriteLine("a / b = " + a / b);
+        }
+    }
+}
+}
