8e4f1b79775643bac3c40b43d0e2731bf6c5d4db
chenzhan2k2 <100663760+chenzhan2k2@users.noreply.github.com>
2023-04-21 21:56:36 +0700

    Tuan_7
    Tuan_7

---
+++ Tuan_7/Bai3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class SinhVien
+{
+    public string MaSV { get; set; }
+    public string TenSV { get; set; }
+    public double DiemTB { get; set; }
+
+    public void NhapThongTin()
+    {
+        Console.Write("Nhap ma so sinh vien: ");
+        MaSV = Console.ReadLine();
+
+        Console.Write("Nhap ten sinh vien: ");
+        TenSV = Console.ReadLine();
+
+        Console.Write("Nhap diem trung binh: ");
+        DiemTB = double.Parse(Console.ReadLine());
+    }
+
+    public void XuatThongTin()
+    {
+        Console.WriteLine("Ma so sinh vien: " + MaSV);
+        Console.WriteLine("Ten sinh vien: " + TenSV);
+        Console.WriteLine("Diem trung binh: " + DiemTB);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SinhVien sv = new SinhVien();
+        sv.NhapThongTin();
+        Console.WriteLine("\nThong tin sinh vien vua nhap:");
+        sv.XuatThongTin();
+
+        Console.ReadKey();
+    }
+}
---
+++ Tuan_7/Bai4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+class SinhVien
+{
+    public string MaSV { get; set; }
+    public string TenSV { get; set; }
+    public double DiemTB { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<SinhVien> danhSachSV = new List<SinhVien>();
+
+        // Nhập danh sách sinh viên
+        Console.Write("Nhap so luong sinh vien: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            SinhVien sv = new SinhVien();
+
+            Console.Write("Nhap ma so sinh vien: ");
+            sv.MaSV = Console.ReadLine();
+
+            Console.Write("Nhap ten sinh vien: ");
+            sv.TenSV = Console.ReadLine();
+
+            Console.Write("Nhap diem trung binh: ");
+            sv.DiemTB = double.Parse(Console.ReadLine());
+
+            danhSachSV.Add(sv);
+        }
+
+        // Tìm kiếm sinh viên theo mã số
+        Console.Write("Nhap ma so sinh vien can tim: ");
+        string maSo = Console.ReadLine();
+
+        SinhVien svTimThay = danhSachSV.Find(sv => sv.MaSV == maSo);
+
+        if (svTimThay == null)
+        {
+            Console.WriteLine("Khong tim thay sinh vien co ma so " + maSo);
+        }
+        else
+        {
+            Console.WriteLine("Thong tin sinh vien co ma so " + maSo);
+            Console.WriteLine("Ten sinh vien: " + svTimThay.TenSV);
+            Console.WriteLine("Diem trung binh: " + svTimThay.DiemTB);
+        }
+
+        Console.ReadKey();
+    }
+}
